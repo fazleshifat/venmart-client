d70d66a6141d12d9aca821bdbd2a7f9569c8e13e
fazleshifat <mailbox189.shifat@gmail.com>
2025-06-09 22:46:40 +0600

    secured firebase auth key

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCa-aoJWp8xm15K55zyLaHdPVgk1-XV3Lo",
-    authDomain: "venmart-f0b2a.firebaseapp.com",
-    projectId: "venmart-f0b2a",
-    storageBucket: "venmart-f0b2a.firebasestorage.app",
-    messagingSenderId: "483602565621",
-    appId: "1:483602565621:web:cb475f45cdfdb6d413b8bc"
+    apiKey:import.meta.env.VITE_apiKey,
+    authDomain:import.meta.env.VITE_authDomain,
+    projectId:import.meta.env.VITE_projectId,
+    storageBucket:import.meta.env.VITE_storageBucket,
+    messagingSenderId:import.meta.env.VITE_messagingSenderId,
+    appId:import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
